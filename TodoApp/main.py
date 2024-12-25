72c9831074ec438c6014ed3a92a710c7856dd483
Abinash Khatiwada <abinash.khatiwada.a@Abinashs-MacBook-Pro.local>
2024-12-25 23:58:59 +1100

    new end point with path parameter and validation

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from typing import Annotated
 from sqlalchemy.orm import Session
 from . import models
@@ -10,6 +10,15 @@ app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)  # This creates the tables if not already created
 
+db_dependency = Annotated[Session, Depends(get_db)]
+
 @app.get("/")
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}")
+async def read_all(db: db_dependency, todo_id: int = Path(gt = 0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code = 404, detail = "Todo item not found")
